5deb522240faa2205a749456546125888b9d0d58
aadiosap <aadiosap@unal.edu.co>
2022-12-02 08:18:28 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -122,17 +122,19 @@ def pregunta_03():
     # Importe LogisticRegressionCV
     # Importe OneHotEncoder
     # Importe Pipeline
-    from ____ import ____
+    from sklearn.linear_model import LogisticRegressionCV
+    from sklearn.preprocessing import OneHotEncoder
+    from sklearn.pipeline import Pipeline
 
     # Cargue las variables.
-    X_train, _, y_train, _ = pregunta_02()
+    X_train, X_test, y_train, y_test = pregunta_02()
 
     # Cree un pipeline que contenga un estimador OneHotEncoder y un estimador
     # LogisticRegression con una regularización Cs=10
     pipeline = Pipeline(
         steps=[
-            ("____", ____()),
-            ("____", ____(____)),
+            ("OneHotEncoder", OneHotEncoder()),
+            ("LogisticRegression", LogisticRegressionCV(Cs=10)),
         ],
     )
 
